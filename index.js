279080545ab1fd2c151be96195d87ca1b513722f
Marcos Florencio <eptaccio@gmail.com>
2019-02-14 13:59:17 -0200

    add appname

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const { MongoClient } = require('mongodb')
 
+const URLConnection = process.env.MONGODB_URL
+const Database = process.env.MONGODB_DATABASE
+const AppName = process.env.MONGODB_APPNAME
+
 let connection
 
 const defaultConnectionConfig = {
-  ignoreUndefined: true
+  ignoreUndefined: true,
+  useNewUrlParser: true,
+  appname: AppName || Database
 }
 
-const getCollection = collectionName =>
-  connection.collection(collectionName)
+const getCollection = collectionName => connection.collection(collectionName)
 
-const connect = ({ url, database, connectionConfig = defaultConnectionConfig }, done) =>
+const connect = (
+  { url = URLConnection, database = Database, connectionConfig = defaultConnectionConfig },
+  done
+) =>
   MongoClient.connect(url, connectionConfig, (err, client) => {
     if (err) return done(err)
 
